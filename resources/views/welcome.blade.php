d95e833c31bb11475dc2881ee673516e38609ba3
Monsef-Chaibi <xr.monsef@gmail.com>
2023-10-31 18:10:53 +0100

    v12

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1670,33 +1670,7 @@
         </div>
     </div>
 
-    <!--===============================================================================================-->
-    <script src="{{ asset('vendor/jquery/jquery-3.2.1.min.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('vendor/animsition/js/animsition.min.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('vendor/bootstrap/js/popper.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('vendor/select2/select2.min.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('vendor/daterangepicker/moment.min.js') }}"></script>
-    <script src="vendor/daterangepicker/daterangepicker.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('vendor/slick/slick.min.js') }}"></script>
-    <script src="{{ asset('js/slick-custom.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('vendor/parallax100/parallax100.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('vendor/MagnificPopup/jquery.magnific-popup.min.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('vendor/isotope/isotope.pkgd.min.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('vendor/sweetalert/sweetalert.min.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('js/main.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('vendor/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+    
 
        <script>
             $('.gallery-lb').each(function() { // the containers for all your galleries
