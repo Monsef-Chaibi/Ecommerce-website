0a59727ec71d5c7aad154e7ab297ea09e11e168a
Monsef-Chaibi <xr.monsef@gmail.com>
2023-10-31 22:16:46 +0100

    V12

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -4,7 +4,7 @@
 	<title>Shoping Cart</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     @include('include.link')
 
 </head>
@@ -52,34 +52,21 @@
 					<!-- Menu desktop -->
 					<div class="menu-desktop">
 						<ul class="main-menu">
-							<li>
-								<a href="index.html">Home</a>
-								<ul class="sub-menu">
-									<li><a href="index.html">Homepage 1</a></li>
-									<li><a href="home-02.html">Homepage 2</a></li>
-									<li><a href="home-03.html">Homepage 3</a></li>
-								</ul>
-							</li>
+					    <li>
+                                <a href="/">Home</a>
+                            </li>
 
-							<li>
-								<a href="product.html">Shop</a>
-							</li>
+                            <li>
+                                <a href="/product">Product</a>
+                            </li>
 
-							<li class="label1" data-label1="hot">
-								<a href="shoping-cart.html">Features</a>
-							</li>
+                            <li>
+                                <a href="/about">About</a>
+                            </li>
 
-							<li>
-								<a href="blog.html">Blog</a>
-							</li>
-
-							<li>
-								<a href="about.html">About</a>
-							</li>
-
-							<li>
-								<a href="contact.html">Contact</a>
-							</li>
+                            <li>
+                                <a href="/contact">Contact</a>
+                            </li>
 						</ul>
 					</div>
 
@@ -163,37 +150,21 @@
 			</ul>
 
 			<ul class="main-menu-m">
-				<li>
-					<a href="index.html">Home</a>
-					<ul class="sub-menu-m">
-						<li><a href="index.html">Homepage 1</a></li>
-						<li><a href="home-02.html">Homepage 2</a></li>
-						<li><a href="home-03.html">Homepage 3</a></li>
-					</ul>
-					<span class="arrow-main-menu-m">
-						<i class="fa fa-angle-right" aria-hidden="true"></i>
-					</span>
-				</li>
+                <li>
+                    <a href="/">Home</a>
+                </li>
 
-				<li>
-					<a href="product.html">Shop</a>
-				</li>
+                <li>
+                    <a href="/product">Product</a>
+                </li>
 
-				<li>
-					<a href="shoping-cart.html" class="label1 rs1" data-label1="hot">Features</a>
-				</li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
 
-				<li>
-					<a href="blog.html">Blog</a>
-				</li>
-
-				<li>
-					<a href="about.html">About</a>
-				</li>
-
-				<li>
-					<a href="contact.html">Contact</a>
-				</li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
 			</ul>
 		</div>
 
