c12d0d3aa75a9a9ccdcdf848bbdc2a13bf3c2f06
Monsef-Chaibi <xr.monsef@gmail.com>
2023-10-31 22:08:46 +0100

    v12

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 
 Route::get('/product', [Controller::class, 'product']);
 Route::get('/about', [Controller::class, 'about']);
+Route::get('/contact', [Controller::class, 'contact']);
