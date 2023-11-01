fd90be61360aaae0f940207613b6a213e5eabd1c
Monsef-Chaibi <xr.monsef@gmail.com>
2023-11-01 10:31:51 +0100

    v12

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,20 +71,21 @@
 
                     <!-- Icon header -->
                     <div class="wrap-icon-header flex-w flex-r-m">
-                        <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
-                            <i class="zmdi zmdi-search"></i>
-                        </div>
+                        <ul class="main-menu">
+                            <li>
+                                <a href="/login">Login</a>
+                            </li>
+                            <li>
+                                <a href="/register">
+                                    Register
+                                </a>
+                            </li>
 
+                        </ul>
                         <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart"
                             data-notify="2">
                             <i class="zmdi zmdi-shopping-cart"></i>
                         </div>
-
-                        <a href="#"
-                            class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti"
-                            data-notify="0">
-                            <i class="zmdi zmdi-favorite-outline"></i>
-                        </a>
                     </div>
                 </nav>
             </div>
